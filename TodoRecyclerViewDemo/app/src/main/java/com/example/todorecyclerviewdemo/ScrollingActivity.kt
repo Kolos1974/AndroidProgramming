a76e18bc4ab3b27dde052a82441f0899d16c83ed
Kolos1974 <karlovitz.kolos@gmail.com>
2022-08-09 10:29:53 +0200

    TodoRecyclerViewDemo done

--- TodoRecyclerViewDemo/app/src/main/java/com/example/todorecyclerviewdemo/ScrollingActivity.kt
+++ TodoRecyclerViewDemo/app/src/main/java/com/example/todorecyclerviewdemo/ScrollingActivity.kt
@@ -7,20 +7,37 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.example.todorecyclerviewdemo.adapter.TodoAdapter
+import com.example.todorecyclerviewdemo.data.Todo
 import com.example.todorecyclerviewdemo.databinding.ActivityScrollingBinding
+import kotlinx.android.synthetic.main.activity_scrolling.*
 
-class ScrollingActivity : AppCompatActivity() {
+class ScrollingActivity : AppCompatActivity(), TodoDialog.TodoHandler {
 
     private lateinit var binding: ActivityScrollingBinding
+
+    lateinit var todoAdapter: TodoAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityScrollingBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        // binding = ActivityScrollingBinding.inflate(layoutInflater)
+        // setContentView(binding.root)
 
+        setContentView(R.layout.activity_scrolling)
         setSupportActionBar(findViewById(R.id.toolbar))
 
+        todoAdapter = TodoAdapter(this)
+        recyclerTodo.adapter = todoAdapter
+
+        fab.setOnClickListener{
+            TodoDialog().show(supportFragmentManager, "Dialog")
+        }
+
+    }
+
+    override fun todoCreated(todo: Todo) {
+        todoAdapter.addTodo(todo)
     }
 
 
--- TodoRecyclerViewDemo/app/src/main/java/com/example/todorecyclerviewdemo/adapter/TodoAdapter.kt
+++ TodoRecyclerViewDemo/app/src/main/java/com/example/todorecyclerviewdemo/adapter/TodoAdapter.kt
@@ -1,27 +1,59 @@
 package com.example.todorecyclerviewdemo.adapter
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.todorecyclerviewdemo.R
 import com.example.todorecyclerviewdemo.data.Todo
 import kotlinx.android.synthetic.main.todo_row.view.*
 
 
-//class TodoAdapter : RecyclerView.Adapter<TodoAdapter.ViewHolder>{
-class TodoAdapter {
+class TodoAdapter : RecyclerView.Adapter<TodoAdapter.ViewHolder>{
 
     var todoItems = mutableListOf<Todo>(
-        Todo("2022. 07. 16.", false, "Got to out")
+        Todo("2022. 07. 16.", false, "Got to out"),
+        Todo("2022. 07. 22.", false, "Learning"),
+        Todo("2022. 08. 06.", false, "Washing")
     )
 
     val context: Context
-    constructor(context: Context, todoList: List<Todo>) : super() {
+//  constructor(context: Context, todoList: List<Todo>) : super() {
+    constructor(context: Context) : super() {
         this.context = context
         //todoItems.addAll(todoList)
+    }
+
+
+    // Egy adjuk meg, hogy nézzen ki egy sor
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val todoView = LayoutInflater.from(context).inflate(R.layout.todo_row,
+            parent, false)
+        return ViewHolder(todoView)
+    }
+
+    // Az egyes listaelemeknél mit rajzoljon ki
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        var todoItem = todoItems[position]
+
+        holder.tvDate.text = todoItem.createDate
+        holder.cbDone.isChecked = todoItem.done
+        holder.cbDone.text = todoItem.todoText
+    }
+
+    fun addTodo(todo: Todo){
+        todoItems.add(todo)
+        notifyItemInserted(todoItems.lastIndex)
+    }
+
+    override fun getItemCount(): Int {
+        return todoItems.size
     }
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var tvDate = itemView.tvDate
         var cbDone = itemView.cbDone
     }
+
 }
